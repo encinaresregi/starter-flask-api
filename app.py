604a2030c190723bec06a4d7e55f8dacff4e6ba7
encinaresregi <96848420+encinaresregi@users.noreply.github.com>
2024-04-05 05:28:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, make_response
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, world!'
+def home():
+    resp = make_response("hello") #here you could use make_response(render_template(...)) too
+    resp.headers['Sample-header'] = '1103'
+    return resp
