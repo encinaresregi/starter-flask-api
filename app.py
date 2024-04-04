0f9013b5e06e7ea9adf67b4eb81afd21d316e36c
encinaresregi <96848420+encinaresregi@users.noreply.github.com>
2024-04-05 05:40:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    data = ""
     resp = make_response("hello") #here you could use make_response(render_template(...)) too
     resp.headers['Sample-header'] = '1103'
+    resp.headers['Content-Type'] = 'application/x-x509-ca-cert'
+    resp.headers['Content-Length'] = len(data)
+    resp.headers['Connection'] = "close"
     return resp
